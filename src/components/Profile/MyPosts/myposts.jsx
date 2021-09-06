8f3b1a9248d274b78aca879b4cb02fb5abc7d34a
Алексей Евдокимов <alexey.evdokimov@roust.com>
2021-09-06 14:44:31 +0300

    Created ref

--- src/components/Profile/MyPosts/myposts.jsx
+++ src/components/Profile/MyPosts/myposts.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import style from  './myposts.module.css';
 import Post from './Post/Post';
 
@@ -8,16 +9,24 @@ const MyPosts = (props) => {
 
     let postArray = props.postsData.map(post => <Post header={post.header} text={post.text} likeCounts = {post.likeCounts}/>);
 
+    let newPostElement=React.createRef();       //Создаем ссылку на элемент
+
+    let addPost = () =>
+    {
+        let text = newPostElement.current.value; //newPostElement.current - ссылается на свойства реального dom элемента
+        alert(text);
+    }
+
     return (
             <div className={style.myPosts}>
                 My posts                
                 <div>
                     <h3>New post</h3>
                     <div>
-                        <textarea></textarea>
+                        <textarea ref={newPostElement}></textarea>
                     </div>
                     <div>
-                        <button onClick={ ()=> alert('Clicked')}>Add post</button>
+                        <button onClick={ addPost }>Add post</button>
                     </div>
                 </div>
                 {postArray}
