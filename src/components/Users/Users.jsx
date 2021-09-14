f853f479639f2162165d42ca7628932b7feb7bfc
Aleksey Evdokimov <izzzi667@gmail.com>
2021-09-14 22:15:44 +0300

    FIx slide effect

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,37 +4,39 @@ import userPhoto from '../../assets/images/default_avatar.jpg'
 
 const Users = (props) =>
 {
-
-    if (props.users.length===0){
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response =>{
-            props.setUsers(response.data.items);
-        });
+    let getUsers =() =>{
+        if (props.users.length===0){
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response =>{
+                props.setUsers(response.data.items);
+            });
+        }
     }
-    let usersContent= props.users.map(u=>{
-        return (<div key={u.id}>
-            <span>
-                <div><img src={u.photos.small !=null ? u.photos.small: userPhoto}/></div>
-                <div>{
-                    u.followed?
-                    <button onClick={()=>{props.unfollow(u.id)}}>Unfollow</button>:
-                    <button onClick={()=>{props.follow(u.id)}}>Follow</button>
-                }</div>
-            </span>
-            <span>
-                <span><div>{u.name}</div><div>{u.status}</div></span>
-                <span>
-                    <div>{"u.location.city"}</div>
-                    <div>{"u.location.country"}</div>
-                </span>
-            </span>
-        </div>)
-    })
+
+    return <div>
+                <button onClick={getUsers}>Get users</button>
+                {
+                props.users.map(u=> <div key={u.id}>
+                            <span>
+                                <div><img src={u.photos.small !=null ? u.photos.small: userPhoto}/></div>
+                                <div>{
+                                    u.followed?
+                                    <button onClick={()=>{props.unfollow(u.id)}}>Unfollow</button>:
+                                    <button onClick={()=>{props.follow(u.id)}}>Follow</button>
+                                }</div>
+                            </span>
+                            <span>
+                                <span><div>{u.name}</div><div>{u.status}</div></span>
+                                <span>
+                                    <div>{"u.location.city"}</div>
+                                    <div>{"u.location.country"}</div>
+                                </span>
+                            </span>
+                        </div>
+                )}
+            </div>
+
+
     
-
-
-
-    
-    return <div>{usersContent}</div>
 }
 
 export default Users;
