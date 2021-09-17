7c18fc1e5d31eb71d46366850a483985ca5d4863
Aleksey Evdokimov <izzzi667@gmail.com>
2021-09-17 20:10:09 +0300

    Разделены контейнерная и презентационная компоненты

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,54 @@
 import React from 'react';
-import Users from './Users';
 import { connect } from 'react-redux';
 import { followAC, setUsersAC, unfollowAC, setCurrentPageAC, setTotalUsersCountAC } from '../../redux/usersReducer';
+import * as axios from 'axios';
+import Users from './Users';
+
+
+//Классовая компонента - устарелка - не рекомендуется использовать
+class UsersContainerComponent extends React.Component {
+    
+
+    constructor(props){
+        super(props);                   //Если только эта операция - конструктор можно опустить
+
+    }
+
+    getUsers =() =>{
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response =>{
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+        });        
+    }
+
+    componentDidMount(){
+        this.getUsers();                //Вызывается при создании компоненты
+    };
+    
+
+    onPageChanged = (pageNumber) =>{
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response =>{
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+        });        
+
+    }
+
+    render()                                    //React будет вызывать этот метод при отрисовке, возвращать должен jsx
+    {
+
+        return <Users totalUsersCount={this.props.totalUsersCount} 
+            pageSize={this.props.pageSize}
+            currentPage={this.props.currentPage}
+            users={this.props.users}
+            onPageChanged={this.onPageChanged}
+            follow = {this.props.follow}
+            unfollow = {this.props.unfollow}
+        />
+
+    }
+}
 
 
 let mapStateToProps= (state) =>
@@ -37,7 +84,7 @@ let mapDispathcToProps = (dispatch) =>
 
 
 
-const UsersContainer= connect(mapStateToProps,mapDispathcToProps)(Users);
+const UsersContainer= connect(mapStateToProps,mapDispathcToProps)(UsersContainerComponent);
 
 
 
