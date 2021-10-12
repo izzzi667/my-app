af6bdf59083539cc90829109c7f7a056c9d6fa62
izzzi667 <izzzi667@gmail.com>
2021-10-12 14:49:29 +0300

    Add useEffect for profile status

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 
 const ProfileStatusWithHooks =(props) => {
     
     let [editMode, setEditMode] = useState(false);    //возвращает массив из 2 элементов: 1 - значение в State 2 - колбэк-функция, изменяющее это значение
     let [status, setStatus] = useState(props.status);
     
+    useEffect (()=>{
+        setStatus(props.status);
+    }, [props.status]);             //От чего зависит UseEffect
+
     const activateEditMode = () =>{
         setEditMode(true);
     }
